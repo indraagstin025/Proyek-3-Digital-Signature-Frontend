e35b04babb0f80867f17ff882429c8fdbaf94bc2
indraagstin025 <indraagustin012@gmail.com>
2025-11-03 19:01:07 +0700

    push

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const API_BASE_URL = " proyek-3digital-signature-production.up.railway.app/api";
+const API_BASE_URL = "https://proyek-3digital-signature-production.up.railway.app/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
